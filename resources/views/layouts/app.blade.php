a602e002ff1cd896e3c0251e01b2848dfffe1b45
Danick Loic Yagoue <dy@alphabytes.de>
2022-06-08 21:42:50 +0200

    Tab navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>GK-Jewelland | Home</title>
+    <title>GK-Jewelland | @if(isset($navigation)) {{ \Illuminate\Support\Str::of($navigation)->ucfirst() }} @else Home @endif</title>
     <link rel="icon" href="{{ asset('img/gk_logo.png') }}">
 
 
